d7ba7884bb108e006a87c3a80906324d89f14984
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2025-09-27 16:25:34 +0900

    feat: ユーザー編集画面への導線実装と動作確認

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,5 +11,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     # 新規登録の際に、nameのデータ操作を許可する
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
+
+    # アカウント編集の際に、nameのデータ操作を許可する
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
   end
 end
