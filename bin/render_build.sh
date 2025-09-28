f3a28aadf72f22128bc248cfab13c2f9ff9e325c
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2025-09-28 13:53:05 +0900

    add: 保存出来たなかったため再送

--- bin/render_build.sh
+++ bin/render_build.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
+# シェルスクリプトの実行環境を指定
+
+# エラーが発生した場合、すぐにスクリプトの実行を停止し、不完全なデプロイを防ぎます。
 set -o errexit
 
+# GemfileとGemfile.lockに基づき、必要な全てのRubyライブラリをインストールします。
 bundle install
 
+# JavaScript、CSS、画像などの静的ファイル（アセット）を結合・圧縮し、本番環境で使えるように準備します。
 bin/rails assets:precompile
+
+# プリコンパイル後に不要になった一時ファイルなどをクリーンアップし、ディスク容量を節約します。
 bin/rails assets:clean
