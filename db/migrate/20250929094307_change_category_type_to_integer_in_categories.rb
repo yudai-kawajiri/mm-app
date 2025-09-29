187a8cf5f903d280eec7d7eda84cdb613ad49e50
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2025-09-29 19:01:35 +0900

    fix: 'categories'テーブルのカラムのデータ型変更

--- db/migrate/20250929094307_change_category_type_to_integer_in_categories.rb
+++ db/migrate/20250929094307_change_category_type_to_integer_in_categories.rb
@@ -1,4 +1,10 @@
 class ChangeCategoryTypeToIntegerInCategories < ActiveRecord::Migration[8.0]
   def change
+    # カラムの型をstringからintegerに変更する
+    change_column :categories, :category_type, :integer, using: 'category_type::integer', default: 0
+    # usingオプションはPostgreSQLでデータ型を安全に変換するために必要
+    # すでにデータが入っている場合は、default: 0 は不要かもしれませんが
+    # null: falseでデフォルト値がないとエラーになるため、
+    # 'category_type'カラムのnull: false設定を活かすためにも、default: 0 を設定
   end
 end
