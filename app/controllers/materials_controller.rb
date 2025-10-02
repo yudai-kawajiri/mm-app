ef5bfaceac97d9465120024b279565e8dccd6ffa
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2025-10-02 10:50:50 +0900

    feat: 登録・一覧アクションとストロングパラメータの定義

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,5 +1,6 @@
 class MaterialsController < ApplicationController
   def index
+    @materials = Material.all
   end
 
   def show
@@ -11,6 +12,16 @@ class MaterialsController < ApplicationController
   end
 
   def create
+    # 'build'でインスタンスにデータを属性として割り当ててから安全に保存
+    @material = current_user.materials.build(params_params)
+
+    if @material.save
+      # 'redirect_to'で遷移
+      redirect_to materials_path
+    else
+      # 'render'で再表示
+      render :new
+    end
   end
 
   def edit
@@ -20,5 +31,17 @@ class MaterialsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  # ストロングパラメータ設定
+  def material_params
+    params_require(:material).permit(
+    :name, :unit_for_product,
+    :unit_weight_for_product,
+    :unit_for_order,
+    :unit_weight_for_order,
+    :category_id
+  )
   end
 end
