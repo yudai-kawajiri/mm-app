a00b731d271ae55a353d4af06c37714434a0b8bd
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2025-10-08 16:31:48 +0900

    Productsコントローラーに登録から一覧までのアクション設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,23 @@
-class ProductsController < ApplicationController
+class ProductsController < AuthenticatedController
+
   def index
+    @products = current_user.products.all
   end
 
   def new
+    # ユーザーに紐づいた新しいProductインスタンスを準備
+    @product = current_user.products.build
+  end
+
+  def create
+    # ストロングパラメータのデータを使い作成
+    @product = current_user.products.build(product_params)
+    # 詳細画面に遷移（レシピ登録へ）
+    if @product.save
+      redirect_to @product
+    else
+      render :new
+    end
   end
 
   def show
@@ -10,4 +25,16 @@ class ProductsController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :name,
+      :item_number,
+      :price,
+      :category_id,
+  )
+  end
+
 end
