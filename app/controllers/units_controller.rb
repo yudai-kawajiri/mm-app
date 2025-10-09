82c4968545dad89840c694db1e453576df57e4d1
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2025-10-09 15:06:40 +0900

    feat: 単位機能の基盤生成

---
+++ app/controllers/units_controller.rb
@@ -0,0 +1,13 @@
+class UnitsController < AuthenticatedController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+end
---
+++ test/controllers/units_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class UnitsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get units_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get units_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get units_create_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get units_edit_url
+    assert_response :success
+  end
+end
