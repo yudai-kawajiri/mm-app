79313f302b1ff734286226d6d48d6dcd3b1f2b47
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2025-10-09 13:53:22 +0900

    refactor: イーナムの命名変更

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -3,7 +3,7 @@ class Unit < ApplicationRecord
   validates :name, presence: true
   validates :category, presence: true
 
-  # 基本単位と発注単位
-  enum category: { basic: 0, ordering: 1 }
+  # 基本単位と発注単位(basic を production に修正)
+  enum category: { production: 0, ordering: 1 }
 
 end
