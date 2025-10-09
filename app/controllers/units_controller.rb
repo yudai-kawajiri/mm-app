1914c790126d29664490c1a1d2f9e77a172b38c7
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2025-10-09 16:16:33 +0900

    feat: 単位編集画面の実装

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -1,5 +1,7 @@
 class UnitsController < AuthenticatedController
+  before_action :set_unit, only: [:edit, :update]
   def index
+    @units = current_user.units.all
   end
 
   def new
@@ -7,8 +9,31 @@ class UnitsController < AuthenticatedController
   end
 
   def create
+    @unit = current_user.units.build(unit_params)
+    if @unit.save
+      redirect_to units_path
+    else
+      render :new
+    end
   end
 
-  def edit
+  def edit; end
+
+  def update
+    if @unit.update(unit_params)
+      redirect_to units_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def unit_params
+    params.require(:unit).permit(:name, :category)
+  end
+
+  def set_unit
+    @unit = current_user.units.find(params[:id])
   end
 end
