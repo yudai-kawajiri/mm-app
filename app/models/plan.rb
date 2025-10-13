f954b8184b0a7d238b7722ba3a1da8e4f71991d1
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2025-10-13 19:16:34 +0900

    feat: PlanモデルとPloductPlanモデルのバリデーション設定

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,2 +1,15 @@
 class Plan < ApplicationRecord
+  # 関連付け
+  belongs_to :category
+  belongs_to :user
+  has_many :product_plans, dependent: :destroy
+
+  # ネストフォームの設定: ProductPlanの追加・更新・削除を許可
+  # allow_destroy: true => 項目削除用チェックボックスを許可
+  # reject_if: :all_blank => すべてのフィールドが空のレコードを無視
+  accepts_nested_attributes_for :product_plans, allow_destroy: true, reject_if: :all_blank
+
+  # バリデーション
+  validates :plan_date, presence: true
+
 end
