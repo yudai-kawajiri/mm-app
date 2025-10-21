1f7a1ecc695e4781a311787a90b3fb81612eb297
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2025-10-21 16:47:04 +0900

    feat:ProductMaterial の一意性制約の確立

--- app/models/product_plan.rb
+++ app/models/product_plan.rb
@@ -4,5 +4,4 @@ class ProductPlan < ApplicationRecord
 
   # バリデーション (データの整合性のため必須)
   validates :production_count, presence: true, numericality: { greater_than: 0 }
-  validates :product_id, presence: true
 end
