c9275e89e3911a087efa96cbded3fcbec7e873e5
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2025-10-21 18:38:40 +0900

    feat:サイドバーにハイライト機能を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,23 @@ module ApplicationHelper
     return '' if value.blank?
     I18n.t("activerecord.enums.#{record.model_name.i18n_key}.#{attribute}.#{value}")
   end
+
+  # サイドバーのメニュー項目
+  def sidebar_menu_items
+    [
+      { name: t('dashboard.menu.dashboard'), path: authenticated_root_path },
+      { name: t('dashboard.menu.category_management'), path: categories_path },
+      { name: t('dashboard.menu.unit_management'), path: units_path },
+      { name: t('dashboard.menu.material_management'), path: materials_path },
+      { name: t('dashboard.menu.product_management'), path: products_path },
+      { name: t('dashboard.menu.plan_management'), path: plans_path }
+    ]
+  end
+
+  # 現在のページに基づいてアクティブクラスを返す
+  def sidebar_link_class(path)
+    base_class = 'list-group-item list-group-item-action'
+    # current_page?(path) ヘルパーを利用してハイライト判定
+    current_page?(path) ? "#{base_class} active" : base_class
+  end
 end
