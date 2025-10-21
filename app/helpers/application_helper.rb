a6ba440885f2ad6bdb98e1a38789b42a13b7973c
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2025-10-21 20:53:01 +0900

    refactor: 一覧テーブルの表示ロジックをrender_resource_dataヘルパーに分離

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,10 +17,35 @@ module ApplicationHelper
     ]
   end
 
-  # 現在のページに基づいてアクティブクラスを返す
+  # 現在のページに基づいてアクティブクラスを返すメソッド
   def sidebar_link_class(path)
     base_class = 'list-group-item list-group-item-action'
     # current_page?(path) ヘルパーを利用してハイライト判定
     current_page?(path) ? "#{base_class} active" : base_class
   end
+
+  # リソースリストのテーブルセルに表示するデータを整形して返却するメソッド
+  def render_resource_data(resource, column_definition)
+    data = column_definition[:data]
+
+    if data == :image
+      # 1. 画像カラムの処理
+      if resource.image.attached?
+        # 画像が添付されていればサムネイルを表示
+        image_tag resource.image.variant(resize_to_limit: [50, 50]), class: "img-thumbnail"
+      else
+        "-"
+      end
+    elsif data == :price
+      # 2. 金額表示カラムの処理
+      number_to_currency(resource.price)
+    elsif data.respond_to?(:call)
+      # 3. Procの場合の処理
+      data.call(resource)
+    else
+      # 4. カラムの場合の処理
+      resource.send(data)
+    end
+  end
+
 end
