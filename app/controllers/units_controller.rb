cd66cd5706d0cb2c3ae0c939edb35f56fa6fe4b9
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2025-10-27 17:55:07 +0900

    feat: Category/Unitの詳細ページを有効化し、説明フィールドのStrong Parameters/翻訳を追加

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -3,7 +3,7 @@ class UnitsController < AuthenticatedController
   # define_search_params を使って許可するキーを定義
   define_search_params :q, :category
 
-  find_resource :unit, only: [:edit, :update, :destroy]
+  find_resource :unit, only: [:show, :edit, :update, :destroy]
 
   def index
     # ページネーションと検索を適用
@@ -24,6 +24,9 @@ class UnitsController < AuthenticatedController
     respond_to_save(@unit, success_path: units_url)
   end
 
+  def show; end
+
+
   def edit; end
 
   def update
@@ -38,6 +41,6 @@ class UnitsController < AuthenticatedController
   private
 
   def unit_params
-    params.require(:unit).permit(:name, :category)
+    params.require(:unit).permit(:name, :category, :description)
   end
 end
