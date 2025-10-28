01b7c1292fedfd0e58668744ad40822a5aa9d764
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2025-10-28 09:21:02 +0900

    chore: データベーススキーマの更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_27_072205) do
+ActiveRecord::Schema[8.1].define(version: 2025_10_27_072205) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.bigint "record_id", null: false
+    t.string "record_type", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
     t.bigint "byte_size", null: false
     t.string "checksum"
+    t.string "content_type"
     t.datetime "created_at", null: false
+    t.string "filename", null: false
+    t.string "key", null: false
+    t.text "metadata"
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
@@ -43,27 +43,27 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_27_072205) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name", null: false
     t.integer "category_type", default: 0, null: false
-    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.text "description"
+    t.string "name", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["name", "category_type"], name: "index_categories_on_name_and_category_type", unique: true
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "materials", force: :cascade do |t|
-    t.string "name", null: false
-    t.decimal "unit_weight_for_product", precision: 10, scale: 3, null: false
-    t.decimal "unit_weight_for_order", precision: 10, scale: 3, null: false
-    t.bigint "user_id", null: false
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "unit_for_order_id", null: false
     t.text "description"
+    t.string "name", null: false
+    t.bigint "unit_for_order_id", null: false
     t.bigint "unit_for_product_id", null: false
+    t.decimal "unit_weight_for_order", precision: 10, scale: 3, null: false
+    t.decimal "unit_weight_for_product", precision: 10, scale: 3, null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["category_id"], name: "index_materials_on_category_id"
     t.index ["name", "category_id"], name: "index_materials_on_name_and_category_id", unique: true
     t.index ["unit_for_order_id"], name: "index_materials_on_unit_for_order_id"
@@ -72,10 +72,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_27_072205) do
   end
 
   create_table "plan_products", force: :cascade do |t|
+    t.datetime "created_at", null: false
     t.bigint "plan_id", null: false
     t.bigint "product_id", null: false
     t.integer "production_count", null: false
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["plan_id", "product_id"], name: "index_plan_products_on_plan_id_and_product_id", unique: true
     t.index ["plan_id"], name: "index_plan_products_on_plan_id"
@@ -84,24 +84,24 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_27_072205) do
 
   create_table "plans", force: :cascade do |t|
     t.bigint "category_id", null: false
-    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name", null: false
     t.text "description"
+    t.string "name", null: false
     t.integer "status", default: 0, null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["category_id"], name: "index_plans_on_category_id"
     t.index ["name", "category_id"], name: "index_plans_on_name_and_category_id", unique: true
     t.index ["user_id"], name: "index_plans_on_user_id"
   end
 
   create_table "product_materials", force: :cascade do |t|
-    t.bigint "product_id", null: false
-    t.bigint "material_id", null: false
-    t.decimal "quantity", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.bigint "material_id", null: false
+    t.bigint "product_id", null: false
+    t.decimal "quantity", null: false
     t.bigint "unit_id", null: false
+    t.datetime "updated_at", null: false
     t.index ["material_id"], name: "index_product_materials_on_material_id"
     t.index ["product_id", "material_id"], name: "index_product_materials_on_product_id_and_material_id", unique: true
     t.index ["product_id"], name: "index_product_materials_on_product_id"
@@ -109,15 +109,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_27_072205) do
   end
 
   create_table "products", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "price", null: false
-    t.string "item_number", null: false
-    t.integer "status"
-    t.bigint "user_id", null: false
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.text "description"
+    t.string "item_number", null: false
+    t.string "name", null: false
+    t.integer "price", null: false
+    t.integer "status", default: 0, null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["item_number", "category_id"], name: "index_products_on_item_number_and_category_id", unique: true
     t.index ["name", "category_id"], name: "index_products_on_name_and_category_id", unique: true
@@ -125,25 +125,25 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_27_072205) do
   end
 
   create_table "units", force: :cascade do |t|
-    t.string "name", null: false
     t.integer "category", default: 0, null: false
-    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.text "description"
+    t.string "name", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["name", "category"], name: "index_units_on_name_and_category", unique: true
     t.index ["user_id"], name: "index_units_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "name"
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
