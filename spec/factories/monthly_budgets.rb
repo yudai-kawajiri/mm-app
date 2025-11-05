de50be1f4983f2a3f24f28622a55e202e1073700
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2025-11-05 17:19:30 +0900

    feat: MonthlyBudgetのFactory定義を追加

---
+++ spec/factories/monthly_budgets.rb
@@ -0,0 +1,22 @@
+# spec/factories/monthly_budgets.rb
+
+FactoryBot.define do
+  factory :monthly_budget do
+    association :user
+    budget_month { Date.current.beginning_of_month }
+    target_amount { 1000000 }
+    note { "テスト用の予算メモ" }
+
+    trait :this_month do
+      budget_month { Date.current.beginning_of_month }
+    end
+
+    trait :last_month do
+      budget_month { 1.month.ago.beginning_of_month }
+    end
+
+    trait :next_month do
+      budget_month { 1.month.since.beginning_of_month }
+    end
+  end
+end
