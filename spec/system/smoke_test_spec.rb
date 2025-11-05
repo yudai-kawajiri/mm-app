bd44611b473ac502ea556bc83b5cccf419956007
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2025-11-05 21:54:13 +0900

    add: システムスペック用のスモークテストを追加
    - トップページアクセステスト
    - ログインページ表示テスト

---
+++ spec/system/smoke_test_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Smoke Test', type: :system do
+  scenario 'トップページにアクセスできる' do
+    visit root_path
+    expect(page).to have_http_status(:success)
+  end
+
+  # js: true のテストは一旦コメントアウト
+  # scenario 'ログインページが表示される', js: true do
+  #   visit new_user_session_path
+  #   expect(page).to have_content('ログイン')
+  #   expect(page).to have_button('ログイン')
+  # end
+
+  # JavaScript不要なテスト
+  scenario 'ログインページが表示される' do
+    visit new_user_session_path
+    expect(page).to have_content('ログイン')
+    expect(page).to have_field('user[email]')
+    expect(page).to have_field('user[password]')
+  end
+end
