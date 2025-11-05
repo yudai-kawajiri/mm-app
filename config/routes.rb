431f78c5091f1dd6d7fe5f4f7c53017434004471
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2025-11-05 12:38:54 +0900

    feat: システムログ用のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
   # ====================
   namespace :admin do
     resources :users, only: [ :index, :destroy ]
+    resources :system_logs, only: [:index]
   end
 
   # ====================
