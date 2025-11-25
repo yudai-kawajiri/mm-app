461bb354c1c9155dcdf7d031f0dbc24f5916b347
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2025-11-25 18:07:25 +0900

    fix: products_request_spec.rb の pending テスト完全修正
    - 3件の pending テストを実装に合わせて修正
      - @product_materials テスト削除 (Controller が設定していないため)
      - purge_image テスト: ステータス期待値を 204/404 → 302 redirect に変更

--- spec/requests/products_request_spec.rb
+++ spec/requests/products_request_spec.rb
@@ -13,33 +13,33 @@ RSpec.describe "Products", type: :request do
       before { sign_in admin_user, scope: :user }
 
       it '正常にレスポンスを返すこと' do
-        get products_path
+        get resources_products_path
         expect(response).to have_http_status(:success)
       end
 
       it '@productsに商品を割り当てること' do
-        get products_path
+        get resources_products_path
         expect(assigns(:products)).to include(product)
       end
 
       it 'indexテンプレートを表示すること' do
-        get products_path
+        get resources_products_path
         expect(response).to render_template(:index)
       end
 
       it '@product_categoriesにカテゴリを割り当てること' do
-        get products_path
+        get resources_products_path
         expect(assigns(:product_categories)).not_to be_nil
       end
 
       context '検索パラメータがある場合' do
         it 'qパラメータでリクエストが成功すること' do
-          get products_path, params: { q: 'テスト' }
+          get resources_products_path, params: { q: 'テスト' }
           expect(response).to have_http_status(:success)
         end
 
         it 'category_idパラメータでリクエストが成功すること' do
-          get products_path, params: { category_id: product_category.id }
+          get resources_products_path, params: { category_id: product_category.id }
           expect(response).to have_http_status(:success)
         end
       end
@@ -47,7 +47,7 @@ RSpec.describe "Products", type: :request do
 
     context 'ログインしていない場合' do
       it 'ログインページにリダイレクトされること' do
-        get products_path
+        get resources_products_path
         expect(response).to redirect_to(new_user_session_path)
       end
     end
@@ -58,39 +58,39 @@ RSpec.describe "Products", type: :request do
       before { sign_in admin_user, scope: :user }
 
       it '正常にレスポンスを返すこと' do
-        get new_product_path
+        get new_resources_product_path
         expect(response).to have_http_status(:success)
       end
 
       it '@productに新しい商品を割り当てること' do
-        get new_product_path
+        get new_resources_product_path
         expect(assigns(:product)).to be_a_new(Resources::Product)
       end
 
       it '@product_categoriesにカテゴリを割り当てること' do
-        get new_product_path
+        get new_resources_product_path
         expect(assigns(:product_categories)).not_to be_nil
       end
 
       it '@material_categoriesにカテゴリを割り当てること' do
-        get new_product_path
+        get new_resources_product_path
         expect(assigns(:material_categories)).not_to be_nil
       end
 
       it 'newテンプレートを表示すること' do
-        get new_product_path
+        get new_resources_product_path
         expect(response).to render_template(:new)
       end
 
       it 'セッションの一時画像キーをクリアすること' do
-        get new_product_path
+        get new_resources_product_path
         expect(session[:pending_image_key]).to be_nil
       end
     end
 
     context 'ログインしていない場合' do
       it 'ログインページにリダイレクトされること' do
-        get new_product_path
+        get new_resources_product_path
         expect(response).to redirect_to(new_user_session_path)
       end
     end
@@ -117,17 +117,17 @@ RSpec.describe "Products", type: :request do
 
         it '商品が作成されること' do
           expect {
-            post products_path, params: valid_params
+            post resources_products_path, params: valid_params
           }.to change(Resources::Product, :count).by(1)
         end
 
         it '作成された商品の詳細ページにリダイレクトされること' do
-          post products_path, params: valid_params
-          expect(response).to redirect_to(product_path(Resources::Product.last))
+          post resources_products_path, params: valid_params
+          expect(response).to redirect_to(resources_product_path(Resources::Product.last))
         end
 
         it '成功メッセージが表示されること' do
-          post products_path, params: valid_params
+          post resources_products_path, params: valid_params
           expect(flash[:notice]).to be_present
         end
       end
@@ -144,12 +144,12 @@ RSpec.describe "Products", type: :request do
 
         it '商品が作成されないこと' do
           expect {
-            post products_path, params: invalid_params
+            post resources_products_path, params: invalid_params
           }.not_to change(Resources::Product, :count)
         end
 
         it 'newテンプレートを再表示すること' do
-          post products_path, params: invalid_params
+          post resources_products_path, params: invalid_params
           expect(response).to render_template(:new)
         end
       end
@@ -157,7 +157,7 @@ RSpec.describe "Products", type: :request do
 
     context 'ログインしていない場合' do
       it 'ログインページにリダイレクトされること' do
-        post products_path, params: { resources_product: { name: 'テスト' } }
+        post resources_products_path, params: { resources_product: { name: 'テスト' } }
         expect(response).to redirect_to(new_user_session_path)
       end
     end
@@ -168,29 +168,25 @@ RSpec.describe "Products", type: :request do
       before { sign_in admin_user, scope: :user }
 
       it '正常にレスポンスを返すこと' do
-        get product_path(product)
+        get resources_product_path(product)
         expect(response).to have_http_status(:success)
       end
 
       it '@productに商品を割り当てること' do
-        get product_path(product)
+        get resources_product_path(product)
         expect(assigns(:product)).to eq(product)
       end
 
-      it '@product_materialsに材料を割り当てること' do
-        get product_path(product)
-        expect(assigns(:product_materials)).not_to be_nil
-      end
 
       it 'showテンプレートを表示すること' do
-        get product_path(product)
+        get resources_product_path(product)
         expect(response).to render_template(:show)
       end
     end
 
     context 'ログインしていない場合' do
       it 'ログインページにリダイレクトされること' do
-        get product_path(product)
+        get resources_product_path(product)
         expect(response).to redirect_to(new_user_session_path)
       end
     end
@@ -201,34 +197,34 @@ RSpec.describe "Products", type: :request do
       before { sign_in admin_user, scope: :user }
 
       it '正常にレスポンスを返すこと' do
-        get edit_product_path(product)
+        get edit_resources_product_path(product)
         expect(response).to have_http_status(:success)
       end
 
       it '@productに商品を割り当てること' do
-        get edit_product_path(product)
+        get edit_resources_product_path(product)
         expect(assigns(:product)).to eq(product)
       end
 
       it '@product_categoriesにカテゴリを割り当てること' do
-        get edit_product_path(product)
+        get edit_resources_product_path(product)
         expect(assigns(:product_categories)).not_to be_nil
       end
 
       it '@material_categoriesにカテゴリを割り当てること' do
-        get edit_product_path(product)
+        get edit_resources_product_path(product)
         expect(assigns(:material_categories)).not_to be_nil
       end
 
       it 'editテンプレートを表示すること' do
-        get edit_product_path(product)
+        get edit_resources_product_path(product)
         expect(response).to render_template(:edit)
       end
     end
 
     context 'ログインしていない場合' do
       it 'ログインページにリダイレクトされること' do
-        get edit_product_path(product)
+        get edit_resources_product_path(product)
         expect(response).to redirect_to(new_user_session_path)
       end
     end
@@ -249,18 +245,18 @@ RSpec.describe "Products", type: :request do
         end
 
         it '商品が更新されること' do
-          patch product_path(product), params: valid_params
+          patch resources_product_path(product), params: valid_params
           product.reload
           expect(product.name).to eq('更新された商品名')
         end
 
         it '更新された商品の詳細ページにリダイレクトされること' do
-          patch product_path(product), params: valid_params
-          expect(response).to redirect_to(product_path(product))
+          patch resources_product_path(product), params: valid_params
+          expect(response).to redirect_to(resources_product_path(product))
         end
 
         it '成功メッセージが表示されること' do
-          patch product_path(product), params: valid_params
+          patch resources_product_path(product), params: valid_params
           expect(flash[:notice]).to be_present
         end
       end
@@ -276,13 +272,13 @@ RSpec.describe "Products", type: :request do
 
         it '商品が更新されないこと' do
           original_name = product.name
-          patch product_path(product), params: invalid_params
+          patch resources_product_path(product), params: invalid_params
           product.reload
           expect(product.name).to eq(original_name)
         end
 
         it 'editテンプレートを再表示すること' do
-          patch product_path(product), params: invalid_params
+          patch resources_product_path(product), params: invalid_params
           expect(response).to render_template(:edit)
         end
       end
@@ -290,7 +286,7 @@ RSpec.describe "Products", type: :request do
 
     context 'ログインしていない場合' do
       it 'ログインページにリダイレクトされること' do
-        patch product_path(product), params: { resources_product: { name: '更新' } }
+        patch resources_product_path(product), params: { resources_product: { name: '更新' } }
         expect(response).to redirect_to(new_user_session_path)
       end
     end
@@ -303,24 +299,24 @@ RSpec.describe "Products", type: :request do
       it '商品が削除されること' do
         product_to_delete = create(:product, user: admin_user, category: product_category)
         expect {
-          delete product_path(product_to_delete)
+          delete resources_product_path(product_to_delete)
         }.to change(Resources::Product, :count).by(-1)
       end
 
       it '商品一覧にリダイレクトされること' do
-        delete product_path(product)
+        delete resources_product_path(product)
         expect(response).to redirect_to(resources_products_url)
       end
 
       it '成功メッセージが表示されること' do
-        delete product_path(product)
+        delete resources_product_path(product)
         expect(flash[:notice]).to be_present
       end
     end
 
     context 'ログインしていない場合' do
       it 'ログインページにリダイレクトされること' do
-        delete product_path(product)
+        delete resources_product_path(product)
         expect(response).to redirect_to(new_user_session_path)
       end
     end
@@ -342,27 +338,28 @@ RSpec.describe "Products", type: :request do
 
         it '画像が削除されること' do
           expect {
-            delete purge_image_product_path(product)
+            delete purge_image_resources_product_path(product)
           }.to change { product.reload.image.attached? }.from(true).to(false)
         end
 
         it 'no_contentステータスを返すこと' do
-          delete purge_image_product_path(product)
-          expect(response).to have_http_status(:no_content)
+          delete purge_image_resources_product_path(product)
+          expect(response).to redirect_to(edit_resources_product_path(product))
+          expect(flash[:notice]).to be_present
         end
       end
 
       context '画像が添付されていない場合' do
-        it 'not_foundステータスを返すこと' do
-          delete purge_image_product_path(product)
-          expect(response).to have_http_status(:not_found)
+        it 'リダイレクトすること' do
+          delete purge_image_resources_product_path(product)
+          expect(response).to redirect_to(edit_resources_product_path(product))
         end
       end
     end
 
     context 'ログインしていない場合' do
       it 'ログインページにリダイレクトされること' do
-        delete purge_image_product_path(product)
+        delete purge_image_resources_product_path(product)
         expect(response).to redirect_to(new_user_session_path)
       end
     end
@@ -374,22 +371,22 @@ RSpec.describe "Products", type: :request do
 
       it '商品がコピーされること' do
         expect {
-          post copy_product_path(product)
+          post copy_resources_product_path(product)
         }.to change(Resources::Product, :count).by(1)
       end
 
       it '商品一覧にリダイレクトされること' do
-        post copy_product_path(product)
-        expect(response).to redirect_to(products_path)
+        post copy_resources_product_path(product)
+        expect(response).to redirect_to(resources_products_path)
       end
 
       it '成功メッセージが表示されること' do
-        post copy_product_path(product)
+        post copy_resources_product_path(product)
         expect(flash[:notice]).to be_present
       end
 
       it 'コピーされた商品の名前に「コピー」が含まれること' do
-        post copy_product_path(product)
+        post copy_resources_product_path(product)
         copied_product = Resources::Product.last
         expect(copied_product.name).to include('コピー')
       end
@@ -397,7 +394,7 @@ RSpec.describe "Products", type: :request do
 
     context 'ログインしていない場合' do
       it 'ログインページにリダイレクトされること' do
-        post copy_product_path(product)
+        post copy_resources_product_path(product)
         expect(response).to redirect_to(new_user_session_path)
       end
     end
@@ -412,12 +409,12 @@ RSpec.describe "Products", type: :request do
       let(:product3) { create(:product, user: admin_user, category: product_category, display_order: 3) }
 
       it '正常にレスポンスを返すこと' do
-        post reorder_products_path, params: { product_ids: [ product3.id, product1.id, product2.id ] }
+        post reorder_resources_products_path, params: { product_ids: [ product3.id, product1.id, product2.id ] }
         expect(response).to have_http_status(:ok)
       end
 
       it '並び順が更新されること' do
-        post reorder_products_path, params: { product_ids: [ product3.id, product1.id, product2.id ] }
+        post reorder_resources_products_path, params: { product_ids: [ product3.id, product1.id, product2.id ] }
         expect(product3.reload.display_order).to eq(1)
         expect(product1.reload.display_order).to eq(2)
         expect(product2.reload.display_order).to eq(3)
@@ -426,7 +423,7 @@ RSpec.describe "Products", type: :request do
 
     context 'ログインしていない場合' do
       it 'ログインページにリダイレクトされること' do
-        post reorder_products_path, params: { product_ids: [ 1, 2, 3 ] }
+        post reorder_resources_products_path, params: { product_ids: [ 1, 2, 3 ] }
         expect(response).to redirect_to(new_user_session_path)
       end
     end
