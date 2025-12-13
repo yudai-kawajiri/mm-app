e5abbb3f7c0e633e5bf2ea7e4b55f87fe9342eff
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2025-12-13 11:35:56 +0900

    refactor: StoresController の全店舗選択処理を削除
    - switch アクション: nil チェックを削除
    - フォーム画面からのリダイレクト判定を削除（不要になった）

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -5,30 +5,17 @@ class StoresController < AuthenticatedController
 
   # 店舗切替
   #
-  # 【リダイレクト先の判定】
-  # - フォーム画面（/new, /edit）から: 一覧ページへリダイレクト（無限ループ防止）
-  # - その他の画面から: 元のページに留まる
+  # 会社管理者が店舗を切り替える
   def switch
-    store_id = params[:store_id]
+    store = current_user.tenant.stores.find_by(id: params[:store_id])
 
-    if store_id.blank?
-      session[:current_store_id] = nil
-      flash[:notice] = t('stores.switch.all_stores')
+    if store
+      session[:current_store_id] = store.id
+      flash[:notice] = t('stores.switch.success', store_name: store.name)
     else
-      store = current_user.tenant.stores.find_by(id: store_id)
-      if store
-        session[:current_store_id] = store.id
-        flash[:notice] = t('stores.switch.success', store_name: store.name)
-      else
-        flash[:alert] = t('stores.switch.not_found')
-      end
+      flash[:alert] = t('stores.switch.not_found')
     end
 
-    referer = request.referer || authenticated_root_path
-    if referer.match?(/\/(new|\d+\/edit)(\?.*)?$/)
-      redirect_to referer.gsub(/\/(new|\d+\/edit)(\?.*)?$/, '')
-    else
-      redirect_to referer
-    end
+    redirect_to request.referer || authenticated_root_path
   end
 end
