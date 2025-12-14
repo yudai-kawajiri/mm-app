438316e8040d3c2861fcee40b214ccb961bd9054
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2025-12-15 00:30:33 +0900

    feat: 会社申請関連のビューファイルを追加
    - 会社申請フォーム用の new.html.erb を追加
    - 招待承諾フォーム用の accept.html.erb を追加
    - 一貫性のため Devise スタイルのデザインを使用

---
+++ app/mailers/application_request_mailer.rb
@@ -0,0 +1,39 @@
+class ApplicationRequestMailer < ApplicationMailer
+  def invitation_email(application_request)
+    @application_request = application_request
+    @invitation_url = accept_application_requests_url(
+      token: application_request.invitation_token,
+      host: ENV.fetch('APP_HOST', 'localhost:3000'),
+      protocol: ENV.fetch('APP_PROTOCOL', 'http')
+    )
+
+    mail(
+      to: application_request.admin_email,
+      subject: '【MM-App】アプリケーション責任者招待のご案内'
+    )
+  end
+
+  def approval_notification(admin_request)
+    @admin_request = admin_request
+    @user = admin_request.user
+    @login_url = new_user_session_url(
+      host: "#{admin_request.user.tenant.subdomain}.#{ENV.fetch('APP_DOMAIN', 'localhost:3000')}",
+      protocol: ENV.fetch('APP_PROTOCOL', 'http')
+    )
+
+    mail(
+      to: @user.email,
+      subject: '【MM-App】ユーザー登録が承認されました'
+    )
+  end
+
+  def rejection_notification(admin_request)
+    @admin_request = admin_request
+    @user = admin_request.user
+
+    mail(
+      to: @user.email,
+      subject: '【MM-App】ユーザー登録申請について'
+    )
+  end
+end
