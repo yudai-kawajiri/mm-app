cc65bd23ed0d96bcc023d1b9410a1acdc10af3f8
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2025-12-15 21:09:38 +0900

    chore: landing_controller の微調整

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,9 +1,12 @@
+# frozen_string_literal: true
+
 class LandingController < ApplicationController
   layout "application"
 
   def index
-    if user_signed_in?
-      redirect_to authenticated_root_path
+    # ログイン済みユーザーはテナントのダッシュボードにリダイレクト
+    if user_signed_in? && current_user.tenant
+      redirect_to authenticated_root_url(subdomain: current_user.tenant.subdomain), allow_other_host: true
     end
   end
 end
