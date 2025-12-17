8810953e8c07d5ca6fc47ad84f111f047f53215c
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2025-12-17 12:15:29 +0900

    fix: PaperTrailの tenant_id 自動記録とシステムログフィルタリングを改善、翻訳を追加
    - PaperTrailの `tenant_id` 自動記録を実装し、ログに店舗情報を付与
    - 会社管理者は自テナントのログのみ表示されるようにフィルタリングを修正
    - ユーザーフィルタに実際にログがあるユーザーのみ表示するよう改善
    - 残りの各種翻訳キーを追加

--- app/controllers/admin/system_logs_controller.rb
+++ app/controllers/admin/system_logs_controller.rb
@@ -70,17 +70,11 @@ class Admin::SystemLogsController < Admin::BaseController
 
   # アクセス可能なユーザー
   def accessible_users
-    case current_user.role
-    when 'company_admin'
-      if current_store.present?
-        User.where(store_id: current_store.id)
-      else
-        current_user.tenant.users
-      end
-    when 'super_admin'
-      User.all
+    user_ids = accessible_versions.pluck(:whodunnit).compact.uniq
+    if current_user.super_admin?
+      User.where(id: user_ids)
     else
-      User.none
+      User.where(id: user_ids, tenant_id: current_user.tenant_id)
     end
   end
 end
