73c8964346e5648b630dedb5fcb99ee4c20ad423
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2025-12-17 09:35:05 +0900

    feat: 管理者画面の改善
    - Admin::BaseController 追加 (権限チェック共通化)
    - ユーザー編集: 店舗管理者の店舗選択必須化
    - ユーザー詳細: 登録日フィールド削除
    - 翻訳: 管理者関連メッセージの追加

--- app/controllers/admin/admin_requests_controller.rb
+++ app/controllers/admin/admin_requests_controller.rb
@@ -1,80 +1,66 @@
 # frozen_string_literal: true
 
-module Admin
-  class AdminRequestsController < ApplicationController
-    before_action :authenticate_user!
-    before_action :set_admin_request, only: [:show, :approve, :reject]
-    before_action :authorize_company_admin!, only: [:index, :show, :approve, :reject]
-    before_action :authorize_general_user!, only: [:new, :create]
+class Admin::AdminRequestsController < Admin::BaseController
+  before_action :set_admin_request, only: [:show, :approve, :reject]
 
-    def index
-      @admin_requests = AdminRequest
-        .for_tenant(current_tenant)
-        .includes(:user, :store)
-        .recent
-        .page(params[:page])
+  def index
+    @admin_requests = if current_user.company_admin?
+      # 会社管理者: 全店舗のリクエストを表示
+      AdminRequest.for_tenant(current_tenant)
+    elsif current_user.store_admin?
+      # 店舗管理者: 自分の店舗のリクエストのみ
+      AdminRequest.for_tenant(current_tenant).where(store_id: current_user.store_id)
+    else
+      # 一般ユーザー: アクセス不可
+      AdminRequest.none
     end
+    
+    @admin_requests = @admin_requests.includes(:user, :store).recent.page(params[:page])
+  end
 
-    def show
+  def show
+  end
+
+  def new
+    @admin_request = AdminRequest.new
+  end
+
+  def create
+    @admin_request = AdminRequest.new(admin_request_params)
+    @admin_request.user = current_user
+    @admin_request.tenant = current_tenant
+    @admin_request.request_type = :store_admin_request
+
+    if @admin_request.save
+      redirect_to admin_admin_requests_path, notice: t('admin.admin_requests.messages.created')
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    def new
-      @admin_request = AdminRequest.new
+  def approve
+    if @admin_request.approve!
+      redirect_to admin_admin_requests_path, notice: t('admin.admin_requests.messages.approved')
+    else
+      redirect_to admin_admin_requests_path, alert: t('admin.admin_requests.messages.approve_failed')
     end
+  end
 
-    def create
-      @admin_request = AdminRequest.new(admin_request_params)
-      @admin_request.user = current_user
-      @admin_request.tenant = current_tenant
-      @admin_request.request_type = :store_admin_request
-
-      if @admin_request.save
-        redirect_to admin_admin_requests_path, notice: t('admin.admin_requests.messages.created')
-      else
-        render :new, status: :unprocessable_entity
-      end
+  def reject
+    if @admin_request.reject!
+      redirect_to admin_admin_requests_path, notice: t('admin.admin_requests.messages.rejected')
+    else
+      redirect_to admin_admin_requests_path, alert: t('admin.admin_requests.messages.reject_failed')
     end
+  end
 
-    def approve
-      if @admin_request.can_be_approved?
-        @admin_request.approve!(current_user)
-        redirect_to admin_admin_requests_path,
-                    notice: t('admin.admin_requests.messages.approved', name: @admin_request.user.name)
-      else
-        redirect_to admin_admin_requests_path,
-                    alert: t('admin.admin_requests.messages.already_processed')
-      end
-    end
+  private
 
-    def reject
-      reason = params[:admin_request][:rejection_reason]
+  def set_admin_request
+    @admin_request = AdminRequest.find(params[:id])
+  end
 
-      if reason.present? && @admin_request.can_be_approved?
-        @admin_request.reject!(current_user, reason: reason)
-        redirect_to admin_admin_requests_path,
-                    notice: t('admin.admin_requests.messages.rejected')
-      else
-        redirect_to admin_admin_request_path(@admin_request),
-                    alert: t('admin.admin_requests.messages.already_processed')
-      end
-    end
-
-    private
-
-    def set_admin_request
-      @admin_request = AdminRequest.for_tenant(current_tenant).find(params[:id])
-    end
-
-    def admin_request_params
-      params.require(:admin_request).permit(:store_id, :message)
-    end
-
-    def authorize_company_admin!
-      redirect_to root_path, alert: t('errors.unauthorized') unless current_user.company_admin? || current_user.super_admin?
-    end
-
-    def authorize_general_user!
-      redirect_to root_path, alert: t('errors.unauthorized') if current_user.company_admin? || current_user.super_admin?
-    end
+  def admin_request_params
+    params.require(:admin_request).permit(:store_id, :message)
   end
 end
