4234a6753d4479d14b91c4e1ca778f782ced7fd0
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2025-12-21 10:13:53 +0900

    fix: システム管理者チェックをsuperの前に移動
    - ログイン前にメールアドレスからユーザーを検索
    - super_adminならログインを中断してリダイレクト
    - CSRFトークン問題を解決

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -8,14 +8,16 @@ class Users::SessionsController < Devise::SessionsController
 
   # POST /resource/sign_in
   def create
-    super do |resource|
-      # システム管理者のサブドメインチェック(ログイン成功後)
-      if resource.super_admin? && request.subdomain != 'admin'
-        sign_out resource
+    # パラメータからユーザーを取得してチェック(ログイン前)
+    if params[:user] && params[:user][:email].present?
+      user = User.find_by(email: params[:user][:email])
+      if user&.super_admin? && request.subdomain != 'admin'
         flash[:alert] = t('errors.invalid_subdomain_access')
         redirect_to new_user_session_url(subdomain: 'admin'), allow_other_host: true and return
       end
     end
+    
+    super
   end
 
   # DELETE /resource/sign_out
