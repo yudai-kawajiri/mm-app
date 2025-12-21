836fbdec78f83feb83c53baee6493869807ad679
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2025-12-21 10:11:59 +0900

    fix: システム管理者チェックをcreateアクション内に移動(完全版)
    - before_action :check_admin_subdomain_for_login を完全削除
    - check_admin_subdomain_for_login メソッドを完全削除
    - Deviseログイン成功後にsuper do |resource|内でチェック
    - CSRFエラーと2クリック問題を解決

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,42 +1,32 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-  before_action :check_admin_subdomain_for_login, only: [:create]
-
   # GET /resource/sign_in
   # def new
   #   super
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    super do |resource|
+      # システム管理者のサブドメインチェック(ログイン成功後)
+      if resource.super_admin? && request.subdomain != 'admin'
+        sign_out resource
+        flash[:alert] = t('errors.invalid_subdomain_access')
+        redirect_to new_user_session_url(subdomain: 'admin'), allow_other_host: true and return
+      end
+    end
+  end
 
   # DELETE /resource/sign_out
   # def destroy
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-
-  private
-
-  def check_admin_subdomain_for_login
-    # ログイン試行前にチェック
-    return unless params[:user] && params[:user][:email].present?
-    
-    user = User.find_by(email: params[:user][:email])
-    if user&.super_admin? && request.subdomain != 'admin'
-      # ログインを中断
-      flash[:alert] = t('errors.invalid_subdomain_access')
-      redirect_to new_user_session_url(subdomain: 'admin'), allow_other_host: true
-    end
-  end
 end
