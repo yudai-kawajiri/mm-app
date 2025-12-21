3b4b3035252eb22397159d9df65c8887660eed35
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2025-12-22 00:51:38 +0900

    hotfix: パスベース環境でのシステム管理者ログイン制御
    - AdminConfig::ADMIN_PATH_PREFIXで定数化
    - request.pathで/adminパスをチェック
    - /admin以外からのシステム管理者ログインをブロック
    - 本番環境(Render.com)のパスベース構造に対応
    
    Fixes: システム管理者が任意のパスからログインできるセキュリティリスク

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,34 +3,36 @@
 class Users::SessionsController < Devise::SessionsController
   # GET /resource/sign_in
   def new
+    # ログイン画面表示前にチェック
+    if user_signed_in? && current_user.super_admin? && !admin_path?
+      sign_out current_user
+      flash[:alert] = t('errors.invalid_subdomain_access')
+      return redirect_to new_user_session_url, allow_other_host: true
+    end
+
     super
   end
 
   # POST /resource/sign_in
   def create
+    # ログイン前にチェック
+    if params[:user] && params[:user][:email].present?
+      user = User.find_by(email: params[:user][:email])
+      if user&.super_admin? && !admin_path?
+        flash.now[:alert] = t('errors.invalid_subdomain_access')
+        self.resource = resource_class.new(sign_in_params)
+        render :new, status: :unprocessable_entity
+        return
+      end
+    end
+
     super
   end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
   protected
 
-  # ログイン後のリダイレクト先をパスベースに変更
-  def after_sign_in_path_for(resource)
-    if resource.company.present?
-      # パスベース: /c/:company_subdomain/dashboards へリダイレクト
-      company_dashboards_path(company_subdomain: resource.company.subdomain)
-    else
-      # 会社がない場合は選択画面へ
-      select_company_path
-    end
+  # システム管理者用のパスかチェック
+  def admin_path?
+    request.path.start_with?(AdminConfig::ADMIN_PATH_PREFIX)
   end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
