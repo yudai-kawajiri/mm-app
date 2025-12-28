e8bc1c963c3e787ecae2b2391f1035fec0d09c60
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2025-12-28 21:27:46 +0900

    Remove old tenant migrations (renamed to company)

--- db/migrate/20251214134906_add_invitation_token_to_tenants.rb
+++
@@ -1,9 +0,0 @@
-class AddInvitationTokenToTenants < ActiveRecord::Migration[8.1]
-  def change
-    add_column :tenants, :invitation_token, :string
-    add_column :tenants, :invitation_sent_at, :datetime
-    add_column :tenants, :invitation_accepted_at, :datetime
-    add_column :tenants, :company_email, :string
-    add_column :tenants, :company_phone, :string
-  end
-end
