3ce6fc7c108b98dcafc14e463ad97f6632023931
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2026-01-01 10:12:41 +0900

    Add picture-in-picture and YouTube open buttons to video modal
    - video_modal_controller.js に enablePip と openInYouTube メソッドを追加
    - help/index.html.erb のモーダルにフッターとボタンを追加
    - config/locales/help/ja.yml に翻訳キーを追加
    - ピクチャーインピクチャー機能で動画を見ながら作業可能に
    - YouTubeで開く機能を追加

--- app/javascript/controllers/video_modal_controller.js
+++ app/javascript/controllers/video_modal_controller.js
@@ -22,8 +22,8 @@ export default class extends Controller {
     // モーダルのタイトルを設定
     this.titleTarget.textContent = title
 
-    // YouTube埋め込みURLを生成
-    const embedUrl = `https://www.youtube-nocookie.com/embed/${youtubeId}?autoplay=1&rel=0`
+    // YouTube埋め込みURLを生成（enablejsapi=1 を追加）
+    const embedUrl = `https://www.youtube-nocookie.com/embed/${youtubeId}?autoplay=1&rel=0&enablejsapi=1`
     this.iframeTarget.src = embedUrl
 
     // Bootstrap Modalを表示
@@ -33,5 +33,42 @@ export default class extends Controller {
 
   close() {
     this.iframeTarget.src = ""
+  }
+
+  // ピクチャーインピクチャー機能を有効化
+  enablePip() {
+    const iframe = this.iframeTarget
+
+    // iframeの動画要素を取得してPiPを要求
+    try {
+      // YouTube iframe APIにPiPリクエストを送信
+      iframe.contentWindow.postMessage(
+        '{"event":"command","func":"requestPictureInPicture","args":""}',
+        '*'
+      )
+
+      // モーダルを閉じる
+      const modal = bootstrap.Modal.getInstance(this.modalTarget)
+      if (modal) {
+        modal.hide()
+      }
+    } catch (error) {
+      console.error('Picture-in-Picture failed:', error)
+      alert(i18n.t('help.video_modal.pip_not_supported'))
+    }
+  }
+
+  // 新しいタブでYouTubeを開く
+  openInYouTube() {
+    const iframe = this.iframeTarget
+    const src = iframe.src
+
+    // YouTube IDを抽出
+    const match = src.match(/embed\/([^?]+)/)
+    if (match && match[1]) {
+      const youtubeId = match[1]
+      const youtubeUrl = `https://www.youtube.com/watch?v=${youtubeId}`
+      window.open(youtubeUrl, '_blank')
+    }
   }
 }
