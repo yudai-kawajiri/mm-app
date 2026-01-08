38c6beb2419508cf772551da6e0dc7e396765669
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2026-01-08 11:01:25 +0900

    fix: RouterController を public_page として扱う
    - public_page? に controller_name == 'router' を追加
    - ログイン後のリダイレクトで 401 エラーを回避
    - after_sign_in_path_for 経由のアクセスを許可

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,8 @@ class ApplicationController < ActionController::Base
     controller_name == "application_requests" ||
     controller_name == "static_pages" ||
     controller_name == "contacts" ||
-    controller_name == "help"
+    controller_name == "help" ||
+    controller_name == "router"
   end
 
     def set_current_company
