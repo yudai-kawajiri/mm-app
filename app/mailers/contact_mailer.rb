56cf9db3bd3140fbb231c8ca3b2b7ded513b54e1
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2026-01-09 15:45:44 +0900

    fix: 店舗登録、パスワード再設定メール、お問い合わせメールの修正
    - Admin::StoresController: 店舗登録後、詳細ページにリダイレクト (id: @store.id)
    - admin/stores/_form.html.erb: システム管理者が店舗登録時に会社を選択可能に
    - devise/mailer/reset_password_instructions.html.erb: greeting に name パラメータを追加
    - ContactMailer: from を明示的に設定 (ENV.fetch('MAILER_FROM'))
    - config/locales/admin/ja.yml: stores.form.company_prompt を追加 (選択してください)

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -4,6 +4,7 @@ class ContactMailer < ApplicationMailer
 
     # 管理者に送信
     mail(
+      from: ENV.fetch("MAILER_FROM"),
       to: ENV.fetch("ADMIN_EMAIL", "admin@example.com"),
       subject: I18n.t("contact_mailer.subject", subject: @contact.subject),
       reply_to: @contact.email
